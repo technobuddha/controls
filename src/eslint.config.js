7a8cd965aea64f6931ca1533d17fdeee2b4ea282
Phil Hill <phil@technobuddha.com>
2026-03-26 10:49:32 -0400

    project/eslint

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -5,4 +5,7 @@
 import { app } from '@technobuddha/project';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig([app.lint({ files: ['*.config.js'], environment: 'node' })]);
+export default defineConfig([
+  app.lint({ files: ['**/*.ts'], typescript: true }),
+  app.lint({ files: ['**/*.tsx'], typescript: true, react: true }),
+]);
